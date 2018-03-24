400c1eb9d9330ab28178f44f2804f4ae9dc500f0
Yuriy Shandor <yuriyshandor@gmail.com>
2018-03-24 10:00:31 +0200

    Add Header. Mod_02

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
   $('.header-nav__toggler-icon').on('click', function() {
-    $(this).toggleClass('_close');
-    $('.header-nav-bar').slideToggle('0.5s');
+    if ( $(window).width() < 992) {
+      $(this).toggleClass('_close');
+      $('.header-nav-bar').slideToggle('0.5s');
+    }
   });
 });
